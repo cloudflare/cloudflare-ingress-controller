a387c7433d59e0cdf17500fadf9a2c32385cf246
Nate Franzen <nfranzen+github@gmail.com>
2018-09-10 09:02:45 -0700

    run metrics server from argot

--- pkg/tunnel/metrics.go
+++ pkg/tunnel/metrics.go
@@ -1,11 +1,17 @@
 package tunnel
 
 import (
+	"context"
 	"fmt"
 	"net"
+	"net/http"
+	"sync"
 	"time"
 
-	"github.com/cloudflare/cloudflared/metrics"
+	"github.com/cloudflare/cloudflare-ingress-controller/pkg/version"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	// "github.com/cloudflare/cloudflared/metrics"
 	"github.com/cloudflare/cloudflared/origin"
 	"github.com/sirupsen/logrus"
 )
@@ -49,5 +55,52 @@ func ServeMetrics(port int, shutdownC <-chan struct{}, logger *logrus.Logger) (e
 	if err != nil {
 		return err
 	}
-	return metrics.ServeMetrics(metricsListener, shutdownC, logger)
+
+	var wg sync.WaitGroup
+
+	server := &http.Server{
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	metricsPath := "/metrics"
+	http.Handle(metricsPath, promhttp.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte(`<html>
+             <head><title>Argot Metrics</title></head>
+             <body>
+             <h1>Argo Ingress Controller</h1>
+             <p><a href='` + metricsPath + `'>Metrics</a></p>
+             <h2>Build</h2>
+             <pre>` + version.VERSION + `</pre>
+             </body>
+             </html>`))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err = server.Serve(metricsListener)
+	}()
+	logger.WithField("addr", metricsListener.Addr()).Info("Starting metrics server")
+
+	startupDelayTime := time.Millisecond * 500
+	time.Sleep(startupDelayTime)
+
+	<-shutdownC
+	shutdownTimeout := time.Second * 15
+	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	server.Shutdown(ctx)
+	cancel()
+
+	wg.Wait()
+	if err == http.ErrServerClosed {
+		logger.Info("Metrics server stopped")
+		return nil
+	}
+	logger.WithError(err).Error("Metrics server quit with error")
+	return err
 }
